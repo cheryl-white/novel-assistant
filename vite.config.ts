2b42760eb0780ae986b65efef853db24d52b4d58
whiteBeardCat <whiteBeardCat01@gmail.com>
2026-07-10 20:15:58 +0800

    fix:导入路径错误

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/novel-assitant',
+  base: '/novel-assistant/',
   plugins: [react(), tailwindcss()],
 })
